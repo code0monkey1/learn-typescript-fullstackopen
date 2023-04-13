265c311d1938667342e187548faf28b682bbd479
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-13 09:30:28 +0000

    13 Apr 2023, 15:00

--- test.ts
+++ test.ts
@@ -37,7 +37,7 @@ interface Cat {
 }
 
 function isDog(animal: Dog | Cat): animal is Dog {
-  return animal instanceof String;
+  return animal instanceof Number;
 }
 
 const myDog: Dog = { name: 'Fido', breed: 'Labrador' };
