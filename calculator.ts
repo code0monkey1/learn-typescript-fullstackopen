8c4870eb4096008da19827ce13e41131f849992f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-25 06:17:54 +0000

    25 Apr 2023, 11:47

--- calculator.ts
+++ calculator.ts
@@ -17,10 +17,14 @@ const calculator=(a:number,b:number,operation:Operation):number =>{ //number|str
         // if(b===0) return "division cannot be performed"
         if(b===0) throw new Error("division cannot be performed")
         return a/b;
-      default:
-        // this ensure that if a new operation is added to the operation type , a corresponding switch statement is always added to correspond to it , else the exhaustiveCheck will catch it .
-        const exhaustiveCheck: never = operation;
-        return exhaustiveCheck;
+      //(to be use in case of internal values) default:
+      
+      // this ensure that if a new operation is added to the operation type , a corresponding switch statement is always added to correspond to it , else the exhaustiveCheck will catch it .
+      
+      //   const exhaustiveCheck: never = operation;
+      //   return exhaustiveCheck;
+       default: // to be used in case of external values
+      throw new Error('Operation is not multiply, add or divide!');
     }
 
 }
