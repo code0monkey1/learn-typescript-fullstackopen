bff8778aee5c730c756b47533c06ad1461d4ee70
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-26 11:01:44 +0000

    26 Apr 2023, 16:31

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (req , res) => {
   res.send('pong');
 });
 
