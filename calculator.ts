3b532adf42cde5b3f9034ad67c8bfc50f0f17dc9
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-27 11:19:51 +0000

    27 Apr 2023, 16:49

--- calculator.ts
+++ calculator.ts
@@ -7,7 +7,7 @@ export type Operation = 'add'|'subtract'|'multiply'|'divide'
   }
 
   const isOperation =(value :string) :value is Operation =>{
-  return (operation ==='add' || operation === 'subtract' || operation === 'multiply' || operation === 'divide')
+  return (value ==='add' || value === 'subtract' || value === 'multiply' || value === 'divide')
 
   }
   
@@ -18,7 +18,7 @@ export type Operation = 'add'|'subtract'|'multiply'|'divide'
      
   }
 
-  const parseArguments = (args: string[]): MultiplyValues => {
+export const parseArguments = (args: string[]): MultiplyValues => {
     
     if (args.length < 5) throw new Error('Not enough arguments');
     if (args.length > 5) throw new Error('Too many arguments');
@@ -36,32 +36,32 @@ export type Operation = 'add'|'subtract'|'multiply'|'divide'
     }
   }
 
-const calculator=(a:number,b:number,operation:Operation):number =>{ //number|string ( return type if returning string too)
+export const calculator=(a:number,b:number,operation:Operation):number =>{ //number|string ( return type if returning string too)
+    
+      switch(operation){
+        case 'add' :
+          return a+b;
+        case 'subtract':
+          return a-b;
+        case 'multiply':
+          return a*b;
+        case 'divide':
+          // to account for the fact that the function might return a string , we need to put the return values likewise 
+          // if(b===0) return "division cannot be performed"
+          if(b===0) throw new Error("division cannot be performed")
+          return a/b;
+        //(to be use in case of internal values) default:
+        
+        // this ensure that if a new operation is added to the operation type , a corresponding switch statement is always added to correspond to it , else the exhaustiveCheck will catch it .
+        
+        //   const exhaustiveCheck: never = operation;
+        //   return exhaustiveCheck;
 
-    switch(operation){
-      case 'add' :
-        return a+b;
-      case 'subtract':
-        return a-b;
-      case 'multiply':
-        return a*b;
-      case 'divide':
-        // to account for the fact that the function might return a string , we need to put the return values likewise 
-        // if(b===0) return "division cannot be performed"
-        if(b===0) throw new Error("division cannot be performed")
-        return a/b;
-      //(to be use in case of internal values) default:
-      
-      // this ensure that if a new operation is added to the operation type , a corresponding switch statement is always added to correspond to it , else the exhaustiveCheck will catch it .
-      
-      //   const exhaustiveCheck: never = operation;
-      //   return exhaustiveCheck;
+        default: // to be used in case of external values , i.e the value for `operation` is coming from an external api, which is not guaranteed to be of given type.
 
-       default: // to be used in case of external values , i.e the value for `operation` is coming from an external api, which is not guaranteed to be of given type.
-
-      throw new Error('Operation is not multiply, add or divide!');
-      // this is to be caught by the calling code using try-catch
-    }
+        throw new Error('Operation is not multiply, add or divide!');
+        // this is to be caught by the calling code using try-catch
+      }
 
 }
 
@@ -69,6 +69,6 @@ const calculator=(a:number,b:number,operation:Operation):number =>{ //number|str
    
 //    return err instanceof Error
 // }
-const {value1,value2,operation} = parseArguments(process.argv)
+// const {value1,value2,operation} = parseArguments(process.argv)
 
-console.log(calculator(value1,value2 , operation as Operation));
+// console.log(calculator(value1,value2 , operation as Operation));
--- index.ts
+++ index.ts
@@ -2,13 +2,24 @@
 // infer the types
 
 import express from 'express';
-
+import { calculator } from './calculator';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/ping', (_req , res) => {
   res.send('pong');
 });
+
+app.post('/calculator',(req, res) => {
+    
+  const {value_1,value_2,operation} = req.body
+  const result = calculator(value_1, value_2, operation) 
+  res.send({result})
+  
+})
+
 
 
 const PORT = 3003;
